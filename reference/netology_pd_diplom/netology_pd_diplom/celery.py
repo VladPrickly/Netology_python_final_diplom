0a316c6d87876291f4a47b82fbf4e7ffb5a8ea7d
Vladislav <vladislav.goltsev@gmail.com>
2026-04-26 14:32:42 +0300

    add flower service

--- reference/netology_pd_diplom/netology_pd_diplom/celery.py
+++ reference/netology_pd_diplom/netology_pd_diplom/celery.py
@@ -3,8 +3,18 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'netology_pd_diplom.settings')
 
-app = Celery('netology_pd_diplom')
+app = Celery(
+    'netology_pd_diplom',
+    broker='redis://localhost:6379/0',
+    backend='redis://localhost:6379/0',
+)
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
+
+# Расширенная статистика для Flower
+app.conf.update(
+    worker_send_task_events=True,  # Отправлять события задач
+    task_send_sent_event=True,     # Отправлять событие отправки
+)
